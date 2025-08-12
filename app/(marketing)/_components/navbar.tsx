9427c20e382fe8c0b9e5d5799f7859a444f01fb9
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-08-13 04:17:42 +0530

    Auth and db setup completed

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,9 +4,15 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
 
+  const {isAuthenticated, isLoading} = useConvexAuth();
   const scrolled = useScrollTop();
     return(
         <div className={cn(
@@ -14,7 +20,37 @@ export const Navbar = () => {
           scrolled && "border-b shadow-sm"
         )}>
           <Logo />
-          <div className="flex lg:justify-end md:justify-end  w-full items-center gap-x-2">
+          <div className="flex lg:justify-end md:justify-end justify-between w-full items-center gap-x-2">
+            {isLoading && (
+              <Spinner />
+
+            )}
+            {!isAuthenticated && !isLoading && (
+              <>
+                <SignInButton mode="modal">
+                  <Button variant="ghost" size="sm">
+                    Log In
+                  </Button>
+                </SignInButton>
+                <SignInButton mode="modal">
+                  <Button size="sm">
+                    Get Brainy Free
+                  </Button>
+                </SignInButton>
+              </>
+            )}
+            {isAuthenticated && !isLoading && (
+              <>
+                <Button variant="ghost" size="sm" asChild>  
+                  <Link href="/documents">
+                    Enter Jotion
+                  </Link>
+                </Button>
+                <UserButton 
+                  afterSignOutUrl="/" 
+                />
+              </>
+            )}
             <ModeToggle />
           </div>
         </div>
