9d2ea21d5f5fe8f1bab1ba363087504e79d3586d
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-08-18 22:43:36 +0530

    added trash box

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,7 +1,88 @@
 "use client";
 
+import { useParams, useRouter } from "next/navigation";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useState } from "react";
+import { Id } from "@/convex/_generated/dataModel";
+import { toast } from "sonner";
+import { Spinner } from "@/components/spinner";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+
 export const TrashBox = () => {
+
+    const router = useRouter(); 
+    const params = useParams();
+    const documents = useQuery(api.documents.getTrash); 
+    const restore = useMutation(api.documents.restore);
+    const remove = useMutation(api.documents.remove);
+
+    const [search, setSearch] = useState("");
+
+    const filterDocuments = documents?.filter((document)=>{
+        return document.title.toLowerCase().includes(search.toLowerCase());
+    });
+
+    const onClick = (documentId: string) => {
+        router.push(`/docuemnts/${documentId}`);
+    };
+
+    const onRestore = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+        documentId: Id<"documents">,
+    ) => {
+        event.stopPropagation();
+        const promise = restore({ id: documentId });
+
+        toast.promise(promise, {
+            loading: "Restoring Note....",
+            success: "Note Restored!!!",
+            error: "Failed to restore the note"
+        })
+    }
+
+
+    const onRemove = (
+        documentId: Id<"documents">,
+    ) => {
+        const promise = remove({ id: documentId });
+
+        toast.promise(promise, {
+            loading: "Deleting Note....",
+            success: "Note Deleted!!!",
+            error: "Failed to delete the note"
+        });
+
+
+        if(params.documentId === documentId){
+            router.push('/documents')
+        }
+
+    };
+    
+    if(documents === undefined){
+        return(
+            <div className="h-full flex items-center justify-center p-4">
+                <Spinner size="lg"  />
+            </div>
+        )
+    };
+    
     return(
-        <div>Trash Box </div>
-    )
+        <div className="text-sm">
+            <div className="flex items-center gap-x-1 p-2">
+                <Search className="h-4 w-4" />
+                <Input 
+                value={search}
+                onChange={(e) => {e.target.value}}
+                className="h-7 focus-visible:ring-transparent bg-secondary"
+                placeholder="Filter by page title...."
+                />
+            </div>
+            <div className="mt-2 px-1 pb-1">
+                <p className="hidden last:blocktext-sm text-center text-muted-foreground coloour pb-2">No do cumnets found</p>
+            </div>
+        </div>
+    );
 }
