52528278a6ae4122d9fafb79ba175b643e2778b2
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-08-22 16:57:18 +0530

    fix-2

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,82 +1,3 @@
-// "use client";
-
-// import { useMutation, useQuery } from "convex/react";
-// import { api } from "@/convex/_generated/api";
-// import { Id } from "@/convex/_generated/dataModel";
-// import { Toolbar } from "@/components/toolbar";
-// import { Cover } from "@/components/cover";
-// import { Skeleton } from "@/components/ui/skeleton";
-// import { update } from "@/convex/documents";
-// import dynamic from "next/dynamic";
-// import { useMemo } from "react";
-
-// interface DocumentIdPageProps {
-//   params: {
-//     documentId: Id<"documents">;
-//   };
-// };
-
-// const DocumentIdPage = ({
-//   params
-// }:DocumentIdPageProps) => {
-
-//   const Editor = useMemo(()=>dynamic(()=>import("@/components/editor"), { ssr: false }), []);
-
-//   const update = useMutation(api.documents.update);
-
-//   const document = useQuery(api.documents.getById, {
-//     documentId: params.documentId
-//     });
-
-//     if(document === undefined){
-//       return(
-//         <div>
-//           <Cover.Skeleton />
-//           <div className="md:max-w-3xl ;g:max-w-4xl mx-auto mt-10">
-//             <div className="space-y-4 pl-8 pt-4">
-//               <Skeleton className="h-14 w-[50%]" />
-//               <Skeleton className="h-4 w-[80%]" />
-//               <Skeleton className="h-4 w-[40%]" />
-//               <Skeleton className="h-4 w-[60%]" />
-//             </div>
-//           </div>
-//         </div>
-//       )
-//     }
-
-//     if(document === null){
-//       return (
-//         <div>
-//           Not found...    
-//         </div>
-//       )
-//     }
-
-//     const onChange = (content: string) => {
-//       update({
-//         id: params.documentId,
-//         content
-//       });
-//     };
-
-//   return (
-//     <div className="pb-40">
-//       <Cover url={document.coverImage} />
-//       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-//         <Toolbar initialData={document} />
-//         <Editor
-//         onChange={onChange}
-//         initialContent={document.content}
-//         />
-//       </div>
-//     </div>
-//   );
-// };  
-
-// export default DocumentIdPage
-
-
-
 "use client";
 
 import { useMutation, useQuery } from "convex/react";
@@ -85,15 +6,13 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
-// The 'update' import below is unused and can be removed.
-// import { update } from "@/convex/documents"; 
+import { update } from "@/convex/documents";
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
 
 interface DocumentIdPageProps {
   params: {
-    // 1. Correctly type the ID from the URL as a string.
-    documentId: string;
+    documentId: Id<"documents">;
   };
 };
 
@@ -106,41 +25,39 @@ const DocumentIdPage = ({
   const update = useMutation(api.documents.update);
 
   const document = useQuery(api.documents.getById, {
-    // 2. Tell TypeScript to treat the string as an Id for the query.
-    documentId: params.documentId as Id<"documents">
-  });
+    documentId: params.documentId
+    });
 
-  if(document === undefined){
-    return(
-      <div>
-        <Cover.Skeleton />
-        <div className="md:max-w-3xl ;g:max-w-4xl mx-auto mt-10">
-          <div className="space-y-4 pl-8 pt-4">
-            <Skeleton className="h-14 w-[50%]" />
-            <Skeleton className="h-4 w-[80%]" />
-            <Skeleton className="h-4 w-[40%]" />
-            <Skeleton className="h-4 w-[60%]" />
+    if(document === undefined){
+      return(
+        <div>
+          <Cover.Skeleton />
+          <div className="md:max-w-3xl ;g:max-w-4xl mx-auto mt-10">
+            <div className="space-y-4 pl-8 pt-4">
+              <Skeleton className="h-14 w-[50%]" />
+              <Skeleton className="h-4 w-[80%]" />
+              <Skeleton className="h-4 w-[40%]" />
+              <Skeleton className="h-4 w-[60%]" />
+            </div>
           </div>
         </div>
-      </div>
-    )
-  }
+      )
+    }
 
-  if(document === null){
-    return (
-      <div>
-        Not found...      
-      </div>
-    )
-  }
+    if(document === null){
+      return (
+        <div>
+          Not found...    
+        </div>
+      )
+    }
 
-  const onChange = (content: string) => {
-    update({
-      // 3. Also treat the string as an Id here for the mutation.
-      id: params.documentId as Id<"documents">,
-      content
-    });
-  };
+    const onChange = (content: string) => {
+      update({
+        id: params.documentId,
+        content
+      });
+    };
 
   return (
     <div className="pb-40">
@@ -148,8 +65,8 @@ const DocumentIdPage = ({
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
         <Editor
-          onChange={onChange}
-          initialContent={document.content}
+        onChange={onChange}
+        initialContent={document.content}
         />
       </div>
     </div>
