2be04b8a540a8e1066a881c8c8a64127e8479d7f
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-08-22 23:15:49 +0530

    editor changes

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,3 +1,63 @@
+// "use client";
+
+// import {
+//     BlockNoteEditor,
+//     PartialBlock
+// } from "@blocknote/core";
+// import { useBlockNote } from "@blocknote/react";
+// import { BlockNoteView } from "@blocknote/shadcn";
+// import "@blocknote/shadcn/style.css";
+// import "@blocknote/core/style.css";
+// import { useTheme } from "next-themes";
+// import { useEdgeStore } from "@/lib/edgestore";
+
+// interface EditorProps {
+//     onChange: (value: string) => void;
+//     initialContent?: string;
+//     editable?: boolean;
+// }
+
+// const Editor = ({
+//     onChange,
+//     initialContent,
+//     editable
+// }: EditorProps) => {
+//     const { resolvedTheme } = useTheme();
+//     const { edgestore } = useEdgeStore();
+
+//     const handleUpload= async (file: File) => {
+//         const response = await edgestore.publicFiles.upload({
+//             file
+//         });
+
+//         return response.url;
+//     }
+
+
+//     const editor: BlockNoteEditor = useBlockNote({
+//         editable,
+//         initialContent: initialContent
+//             ? JSON.parse(initialContent) as PartialBlock[]
+//             : undefined,
+//         uploadFile: handleUpload
+//     });
+
+//     return (
+//         <div className="editor-wrapper">
+//             <BlockNoteView
+//                 editor={editor}
+//                 onChange={() => {
+//                     onChange(JSON.stringify(editor.document, null, 2));
+//                 }}
+//                 theme={resolvedTheme === "dark" ? "dark" : "light"}
+//             />
+//         </div>
+//     )
+// }
+
+// export default Editor;
+
+
 "use client";
 
 import {
@@ -25,7 +85,7 @@ const Editor = ({
     const { resolvedTheme } = useTheme();
     const { edgestore } = useEdgeStore();
 
-    const handleUpload= async (file: File) => {
+    const handleUpload = async (file: File) => {
         const response = await edgestore.publicFiles.upload({
             file
         });
@@ -33,9 +93,8 @@ const Editor = ({
         return response.url;
     }
 
-
     const editor: BlockNoteEditor = useBlockNote({
-        editable,
+        // 'editable' has been removed from here
         initialContent: initialContent
             ? JSON.parse(initialContent) as PartialBlock[]
             : undefined,
@@ -46,6 +105,8 @@ const Editor = ({
         <div className="editor-wrapper">
             <BlockNoteView
                 editor={editor}
+                // The 'editable' prop is now correctly passed here
+                editable={editable}
                 onChange={() => {
                     onChange(JSON.stringify(editor.document, null, 2));
                 }}
